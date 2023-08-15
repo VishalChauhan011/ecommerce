262935f04eb95adbc713ca295d2ae1a39d0fef13
VishalChauhan011 <vishalchauhan46750@gmail.com>
2023-08-15 23:44:42 +0530

    Toast error fix

--- src/screens/ProductDetails.jsx
+++ src/screens/ProductDetails.jsx
@@ -30,7 +30,22 @@ const ProductDetails = () => {
     dispatch(addToCart({ id, name, price, description, reviews }));
   };
 
+  const notify = () => {
+    toast.success("Success. Check your cart!", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+};
+
   return (
+    <>
+    <ToastContainer />
     <div className="flex flex-row px-[100px] justify-center ">
       <div className="flex flex-col  ">
         <ProductDetailsCorousel />
@@ -52,58 +67,6 @@ const ProductDetails = () => {
           </div>
         </div>
       </div>
-     const notify = () => {
-        toast.success("Success. Check your cart!", {
-            position: "bottom-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-        });
-    };
-
-    return (
-        <>
-        <ToastContainer />
-        <div className='flex flex-row '>
-            <div>
-                <ProductDetailsCorousel />
-            </div>
-
-            <div className='flex flex-col'>
-                <p className='font-manrope text-[36px] font-[800] text-black leading-[52px] tracking-[3.6px] mt-[54px] '>{name}</p>
-                <p className='font-manrope text-[16px] font-[600] text-black/50 leading-[23px] tracking-[1.52px] ' >{description}</p>
-                <div className='flex flex-row items-center'>
-                    {ratingStars}
-                    <p className='font-manrope text-[12px] text-black/70 tracking-[1.2px] leading-[17px]'>({reviews})</p>
-                </div>
-                <div className='flex flex-col w-[400px] h-[66px] rounded-[10px] border border-solid border-black my-[28px] justify-center '>
-                    <p className='font-manrope text-[20px] font-[700] leading-[29px] tracking-[2px] ml-[23px]  '>MRP: &#8377;{price}</p>
-                    <p className='font-manrope text-[10px] font-[700] leading-[14.5px] tracking-[1px] ml-[23px] '>(Inc. all taxes)</p>
-                </div>
-                <p className='font-manrope text-[20px] font-[600] leading-[29px] tracking-[2px] mb-[20px] '>Colour</p>
-                <div className='flex flex-col w-[190px] h-[93px] rounded-[10px] border border-black/50 border-solid items-center justify-center '>
-                    <div className='w-[29px] h-[29px] rounded-full bg-gray-400 mb-[8px] '></div>
-                    <p className='font-manrope text-[12px] font-[600] leading-[17.4px] tracking-[1.2px] text-black '>Space Grey</p>
-                </div>
-
-                <div>
-                    <button className='w-[190px] h-[52px] rounded-[10px] bg-black/90 text-white
-                     font-manrope text-[16px] font-[700] leading-[23px]
-                      tracking-[1.6px] mt-[20px] cursor-pointer ' onClick={() => {
-                        handleAddToCart();
-                        notify();
-                      }}
-                        >Add to Cart</button>
-                </div>
-            </div>
-        </div>
-        </>
-    )
-}
 
       <div className="flex flex-col">
         <p className="font-manrope text-[36px] font-[800] text-black leading-[52px] tracking-[3.6px] mt-[54px] ">
@@ -141,13 +104,17 @@ const ProductDetails = () => {
             className="w-[400px] h-[60px] rounded-full bg-black/90 text-white
                      font-manrope text-[16px] font-[700] leading-[23px]
                       tracking-[1.6px] mt-[55px] cursor-pointer "
-            onClick={handleAddToCart}
+                      onClick={() => {
+                        handleAddToCart();
+                        notify();
+                      }}
           >
             Add to Cart
           </button>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
